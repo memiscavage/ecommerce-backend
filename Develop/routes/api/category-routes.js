be9e814d63e3d72c4073802dbeba7c96f831d8c2
Mariel Miscavage <memiscavage@gmail.com>
2024-05-26 14:12:44 -0500

    added get endpoints for router using async await function

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,14 +3,33 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+try {
+  const categories = await Category.findAll({
+    include: [{model: Product}]
+  })
+  res.status(200).json(categories)
+} catch (error) {
+  res.status(500).json(error)
+}
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+try {
+  const category = await Category.findByPk(req.params.id, {
+    include: [{model: Product}]
+  })
+  if (!category) {
+    return res.status(404).json({message: "No category found"})
+  }
+  res.status(200).json(category)
+} catch (error) {
+  res.status(500).json(error)
+}
 });
 
 router.post('/', (req, res) => {
