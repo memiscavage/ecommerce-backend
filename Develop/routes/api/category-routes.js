af0b952b72ced9ef53d05ea302ae20d47cedc278
Mariel Miscavage <memiscavage@gmail.com>
2024-05-29 14:11:36 -0500

    added get and post routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -32,8 +32,14 @@ try {
 }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+try {
+  const newCategory = await category.create(req.body);
+  res.status(200).json(newCategory)
+} catch (error) {
+  res.status(500).json(error)
+}
 });
 
 router.put('/:id', (req, res) => {
