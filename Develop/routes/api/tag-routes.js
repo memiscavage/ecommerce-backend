03f192c066c1ae5ae6e87d8731c1193eb4266cd9
Mariel Miscavage <memiscavage@gmail.com>
2024-05-29 14:11:48 -0500

    added get routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,14 +3,33 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+try {
+  const tags = await Tag.findAll({
+    include: [{model: Product}]
+  })
+  res.status(200).json(tags)
+} catch (error) {
+  res.status(500).json(error)  
+}
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+try {
+  const tag = await Tag.findByPk(req.params.id, {
+    include: [{model: Product}]
+  })
+  if (!tag) {
+    return res.status(404).json({message: "No tag found"})
+  }
+  res.status(200).json(tag)
+} catch (error) {
+  res.status(500).json(error)
+}
 });
 
 router.post('/', (req, res) => {
