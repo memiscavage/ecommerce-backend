9aac9f18dd5f8c9e39e363f9534a7b991f8bb4f2
Mariel Miscavage <memiscavage@gmail.com>
2024-05-29 15:41:41 -0500

    added put and delete routes to update tags

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -35,19 +35,37 @@ try {
 router.post('/', async (req, res) => {
   // create a new category
 try {
-  const newCategory = await category.create(req.body);
+  const newCategory = await Category.create(req.body);
   res.status(200).json(newCategory)
 } catch (error) {
   res.status(500).json(error)
 }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  const updateCategory = await Category.update(
+    {where: {
+      id: req.params.id,
+    }}
+  );
+  res.json(updateCategory);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+try {
+  const newCategory = await Category.destroy({
+     where: { id: req.params.id }
+    });
+  if (!newCategory) {
+    res.status(404).json({ message: 'No category with this id found.' });
+    return;
+    }
+    res.status(200).json(newCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
